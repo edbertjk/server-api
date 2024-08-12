2fedd2defc2d2fe0ffb727bef329a6720e5e6ab8
edbertjk <qeuans12@gmail.com>
2024-08-12 22:33:34 +0700

    Add Project

--- cmd/main.go
+++ cmd/main.go
@@ -1,14 +1,43 @@
 package main
 
 import (
+	"database/sql"
+	"fmt"
 	"log"
 
 	"github.com/edbertjk/server-api/cmd/api"
+	"github.com/edbertjk/server-api/config"
+	"github.com/edbertjk/server-api/db"
+	"github.com/go-sql-driver/mysql"
 )
 
-func main(){
-	server := api.NewAPIServer(":8080", nil)
-	if err := server.Run(); err != nil{
+func main() {
+	db, err := db.NewMySQLStorate(mysql.Config{
+		User:                 config.Envs.DBUser,
+		Passwd:               config.Envs.DBPassword,
+		Addr:                 config.Envs.DBAddress,
+		DBName:               config.Envs.DBName,
+		Net:                  "tcp",
+		AllowNativePasswords: true,
+		ParseTime:            true,
+	})
+
+	if err != nil {
 		log.Fatal(err)
 	}
+
+	initStorage(db)
+
+	server := api.NewAPIServer(":8080", db)
+	if err := server.Run(); err != nil {
+		log.Fatal(err)
+	}
+}
+
+func initStorage(db *sql.DB) {
+	err := db.Ping()
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("DB: Successfully Connect")
 }
--- service/user/routes.go
+++ service/user/routes.go
@@ -3,26 +3,35 @@ package user
 import (
 	"net/http"
 
+	"github.com/edbertjk/server-api/types"
+	"github.com/edbertjk/server-api/utils"
 	"github.com/gorilla/mux"
 )
 
 type Handler struct {
-
+	store *types.UserStore
 }
 
-func NewHandler() *Handler{
+func NewHandler() *Handler {
 	return &Handler{}
 }
 
-func (h *Handler) RegisterRoutes(router *mux.Router){
+func (h *Handler) RegisterRoutes(router *mux.Router) {
 	router.HandleFunc("/login", h.handleLogin).Methods("POST")
 	router.HandleFunc("/register", h.handleRegister).Methods("POST")
 }
 
-func (h *Handler) handleLogin(w http.ResponseWriter, r *http.Request){
-	w.Write("")
+func (h *Handler) handleLogin(w http.ResponseWriter, r *http.Request) {
+
 }
 
-func (h *Handler) handleRegister(w http.ResponseWriter, r *http.Request){
+func (h *Handler) handleRegister(w http.ResponseWriter, r *http.Request) {
+	//get JSON payload
+	var payload types.RegisterUserPayload
+	if err := utils.ParseJSON(r, payload); err != nil{
+		utils.WriteError(w, http.StatusBadRequest, err)
+	}
+	//check if the user exists
+	//we doesnt make user
 
 }
